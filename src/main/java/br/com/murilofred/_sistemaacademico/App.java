8e8e4ba8cdda036213c499b07d9e2fd44dcc0fb8
Murilo Frederico <murilofredericogarcez@gmail.com>
2023-10-02 20:57:40 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/murilofred/_sistemaacademico/App.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.murilofred._sistemaacademico;
+
+/**
+ *
+ * @author 823153149
+ */
+public class App{
+
+    public static void main(String[] args) {
+        
+        for(int a = 1; a > 0; a++){
+
+        System.out.println(a + " ");
+    }
+}
+}
